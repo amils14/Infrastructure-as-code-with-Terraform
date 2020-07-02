571376a91ddf6b904f1aef6b1600b77a87889b62
Amil Sharma <sharma.amil2010@gmail.com>
2020-07-02 18:37:43 +0530

    removed spaces

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ print "<b>Private Ip of the EC2 instance is </b>";
 
 print `ifconfig eth0 | grep "inet" | cut -d ':' -f 2 | cut -d '' -f 1 | cut -b 14-25`;
 
-print "<h3> RAM Usage \n</h3>";
+print "<h3>RAM Usage \n</h3>";
 print `free -m`;
 
 ?>
